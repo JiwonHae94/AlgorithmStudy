e550c758694200bb192d97269f15d20e9f970944
JiwonHae <hjiwon1994@gmail.com>
2021-09-24 01:52:54 +0900

    sample question and answer

---
+++ coding_test/sample_question.py
@@ -0,0 +1,8 @@
+
+# -----------------------
+# category : 연습문제, array
+# Q : 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+# url : https://programmers.co.kr/learn/courses/30/lessons/12944
+# 설명 : 평균 = list의 합 / list안의 item 수
+def solution(arr):
+    return sum(arr) / len(arr)
