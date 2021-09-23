05c0a1e3c7c650a77f53ec1541abc948560dfc3f
JiwonHae <hjiwon1994@gmail.com>
2021-09-24 01:57:11 +0900

    failed to ans

--- coding_test/sample_question.py
+++ coding_test/sample_question.py
@@ -6,3 +6,14 @@
 # 설명 : 평균 = list의 합 / list안의 item 수
 def solution(arr):
     return sum(arr) / len(arr)
+
+
+##########
+# 정해진 기간 (max 이릍) 내 답변을 풀지 못한 경우
+# 아래와 같이 다른 사람의 답변을 복붙 하시고,
+# 커밋 메시지에 표기
+import statistics
+
+def average(list):
+    # 함수를 완성해서 매개변수 list의 평균값을 return하도록 만들어 보세요.
+    return statistics.mean(list)
