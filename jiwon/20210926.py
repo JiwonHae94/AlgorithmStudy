423418858b52c037855f1e1c76b4eb4d2ce2c83a
JiwonHae <hjiwon1994@gmail.com>
2021-09-30 04:36:26 +0900

    work done for 0927 and 0928 on heap and math question

--- jiwon/20210926.py
+++ jiwon/20210926.py
@@ -1,26 +1,25 @@
 
 # -------------------------------------
 # category :
-# Q : 카카오에 신입 개발자로 입사한 "콘"은 선배 개발자로부터 개발역량 강화를 위해 다른 개발자가 작성한 소스 코드를 분석하여 문제점을 발견하고 수정하라는 업무 과제를 받았습니다.
-#     소스를 컴파일하여 로그를 보니 대부분 소스 코드 내 작성된 괄호가 개수는 맞지만 짝이 맞지 않은 형태로 작성되어 오류가 나는 것을 알게 되었습니다.
-#     수정해야 할 소스 파일이 너무 많아서 고민하던 "콘"은 소스 코드에 작성된 모든 괄호를 뽑아서 올바른 순서대로 배치된 괄호 문자열을 알려주는 프로그램을 다음과 같이 개발하려고 합니다.
+# Q : 가로 길이가 Wcm, 세로 길이가 Hcm인 직사각형 종이가 있습니다. 종이에는 가로, 세로 방향과 평행하게 격자 형태로 선이 그어져 있으며,
+#     모든 격자칸은 1cm x 1cm 크기입니다. 이 종이를 격자 선을 따라 1cm × 1cm의 정사각형으로 잘라 사용할 예정이었는데,
+#     누군가가 이 종이를 대각선 꼭지점 2개를 잇는 방향으로 잘라 놓았습니다. 그러므로 현재 직사각형 종이는 크기가 같은 직각삼각형 2개로 나누어진 상태입니다.
+#     새로운 종이를 구할 수 없는 상태이기 때문에, 이 종이에서 원래 종이의 가로, 세로 방향과 평행하게 1cm × 1cm로 잘라 사용할 수 있는 만큼만 사용하기로 하였습니다.
+#     가로의 길이 W와 세로의 길이 H가 주어질 때, 사용할 수 있는 정사각형의 개수를 구하는 solution 함수를 완성해 주세요.
 # url : https://programmers.co.kr/learn/courses/30/lessons/43165
-# 설명 : dfs 를 통해 모든 조합 탐색
+# 설명 : 수학
 # --------------------------------------
 # space complexity : 0 -> 추가 메모리 사용 없음
-# time complexity : O(n) -> sum(list)할때 루프
+# time complexity : O(1) -> sum(list)할때 루프
+from math import gcd
 
-def solution(numbers, target):
-    return dfs([], numbers, target, len(numbers))
+def solution(w, h):
+    answer = w * h
 
-def dfs(temp, numbers, target, k):
-    answer = 0
+    def get_unusable_box():
+        """
+        가로 + 세로 - 최대공약수
+        """
+        return w + h - gcd(w, h)
 
-    if len(temp) == k:
-        return int(sum(temp) == target)
-
-    answer += dfs(temp + [numbers[0]], numbers[1:], target, k)
-    answer += dfs(temp + [-numbers[0]], numbers[1:], target, k)
-    return answer
-
-print("ans : ", solution([1, 1, 1, 1, 1], 3))
+    return answer - get_unusable_box()
