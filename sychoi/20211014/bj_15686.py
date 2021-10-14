2f90601d505e217ad96b0f19195d63e952931890
SoyeongChoi <csyeong9619@gmail.com>
2021-10-15 00:26:59 +0900

    수정

--- sychoi/20211014/bj_15686.py
+++ sychoi/20211014/bj_15686.py
@@ -1,7 +1,7 @@
 def distance(h, c):
     x1, y1 = h
     x2, y2 = c
-    
+
     return abs(x1-x2) + abs(y1-y2)
 
 def find_idx(n, city):
@@ -10,22 +10,8 @@ def find_idx(n, city):
         for j in range(len(city[i])):
             if city[i][j] == n:
                 idxs.append((i, j))
-                
+
     return idxs
-
-def sum_distance(chicken, houses):
-    dist = 0
-    for house in houses:
-        dist += distance(house, chicken)
-    return dist
-
-def min_distance(chickens, house):
-    min_value = 987654321
-
-    for chicken in chickens:
-        if min_value > distance(house, chicken):
-            min_value = distance(house, chicken)
-    return min_value
 
 if __name__=='__main__':
     n, m = map(int, input().split())
@@ -34,16 +20,22 @@ if __name__=='__main__':
         city.append(list(map(int, input().split())))
 
     houses = find_idx(1, city)
-    chickens = find_idx(2, city)
+    chickens_idx = find_idx(2, city)
 
-    chicken_dict = {}
-    for chicken in chickens:
-        chicken_dict[chicken] = sum_distance(chicken, houses)
-    sorted_dict = sorted(chicken_dict.items(), key = lambda item: item[1])
-    sorted_dict = [x[0] for x in sorted_dict]
+    from itertools import combinations
+    chickens_ = list(combinations(chickens_idx, m))
+    answer = 9876543
+    for chickens in chickens_:
+        temp = 0
+        for house in houses:
+            min_v = 8765432
+            for chicken in chickens:
+                if min_v > distance(house, chicken):
+                    min_v = distance(house, chicken)
+            temp += min_v
 
-    chickens_ = sorted_dict[:m]
-    answer = 0
-    for house in houses:
-        answer += min_distance(chickens_, house)
+        if answer > temp:
+            answer = temp
+
+
     print(answer)
